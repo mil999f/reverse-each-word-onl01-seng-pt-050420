153ed9da30953d8bac9ffcb58beb8d5ec9d73695
Milly Flores <miflo0h@gmail.com>
2020-05-16 16:45:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence1)
-  sentence1.each do |sentence1|
-  puts " Hello there, and how are you?"
-  end
-  sentence1
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
 end
